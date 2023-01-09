43e3ca1d52662654092f50be4f207b38527b200f
Noemie Jankowski <noemiejankowski@MacBook-Air-de-Noemie.local>
2023-01-09 21:49:22 +0100

    index

--- front/script/script.js
+++ front/script/script.js
@@ -3,14 +3,30 @@ fetch("http://localhost:3000/api/products")
 .then((data) => {
     console.log(data)
     for  (let i = 0 ; i < data.length; i++) {
+        let link = document.createElement('a')
+        link.href = "./product.html?id=" + data[i]._id
+
         let article = document.createElement('article')
         let h3 = document.createElement('h3')
+        h3.innerHTML = data[i].name
+        h3.className = "productName"
+
+        let p =document.createElement('p')
+        p.innerHTML=data[i].description
+        p.className = "productDescriptions"
+
         let img = document.createElement('img')
         img.src = data[i].imageUrl
+        img.alt = data[i].altTxt
+        
         article.appendChild(img)
-        h3.innerHTML = data[i].name
         article.appendChild(h3)
-        document.getElementById('items').appendChild(article)
+        article.appendChild(p)
+
+        link.appendChild(article)
+        document.getElementById('items').appendChild(link)
     } 
     // addProducts(data)
 })
+
+
